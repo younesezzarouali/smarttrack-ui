32844fa683e26f5a220f10f222448ccd51005857
younes_ezzarouali <youness.ezzarouali@gmail.com>
2025-09-22 17:15:34 +0200

    first message

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(NgbModule)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseModalComponent } from './components/expense-modal/expense-modal';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ExpenseSummaryComponent } from "./components/expense-summary/expense-summary";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ExpenseSummaryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  constructor(private modalService: NgbModal) {
+  }
+
   year = new Date().getFullYear();
+
+  openExpenseModal() {
+    this.modalService.open(ExpenseModalComponent)
+  }
 }

d0a2ce1761e26df34906411b54e7f6bc4b1c4c6a
younes_ezzarouali <youness.ezzarouali@gmail.com>
2025-09-25 15:43:59 +0200

    url ssl

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ac3673dbdb4d940758d8cc00f6dd1d9a-1669720453.eu-west-3.elb.amazonaws.com'  // ✅ ton back en prod (exemple)
+    apiUrl: 'https://app.ezzium.fr'  // ✅ ton back en prod (exemple)
   };

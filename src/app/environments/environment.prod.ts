74c221c21dc178fcf1f51427c0bc815116828b58
Younes Ezzarouali <younes.ezzarouali@gmail.com>
2026-02-26 22:35:43 +0000

    build: link frontend to the real AWS Lambda URL

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://app.ezzium.fr'  // ✅ ton back en prod (exemple)
+    apiUrl: 'https://y6rvhnzpuao66pkudifw6fprny0gynzi.lambda-url.eu-west-3.on.aws'
   };
